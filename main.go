865a7e49a4469609ef2eb056f1140a20721dc92d
Sean DuBois <sean@siobud.com>
2014-06-14 18:32:46 +0000

    Add config file, Addr is configured via config

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"os"
 )
 
+type Configuration struct {
+	Addr string
+}
+
 func main() {
+	file, fileErr := os.Open("config.json")
+
+	if fileErr != nil {
+		fmt.Println("failed to open config:", fileErr)
+		os.Exit(1)
+	}
+
+	configuration := new(Configuration)
+	configErr := json.NewDecoder(file).Decode(configuration)
+
+	if configErr != nil {
+		fmt.Println("config error:", configErr)
+		os.Exit(1)
+	}
+
 	r := mux.NewRouter()
 	GlobalHandler(r.PathPrefix("/global").Subrouter())
 	http.Handle("/", r)
-	http.ListenAndServe(":5000", nil)
+	http.ListenAndServe(configuration.Addr, nil)
 }
